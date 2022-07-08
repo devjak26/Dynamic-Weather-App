9ec4b13b1e4043861a6c1bf531f5585fefef16b3
devendra kumar <91061504+devjak26@users.noreply.github.com>
2022-07-09 00:59:34 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-    openWeatherMap: {
-        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "11ba4d79cdbb23f28bbb531cd97c26d7"
-    }
-}
-
-module.exports = constants;
